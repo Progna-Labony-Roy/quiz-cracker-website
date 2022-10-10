70fe1d21b393c530d291eae43eabea9fe9b38751
Progna_Labony_Roy <labony.progna@gmail.com>
2022-10-11 00:10:36 +0600

    blog page added

--- quiz-website/src/components/Blog/Blog.js
+++ quiz-website/src/components/Blog/Blog.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-            <h1>This is blog</h1>
+  return (
+    <div>
+      <section className="dark:bg-gray-800 dark:text-gray-100">
+        <div className="container flex flex-col justify-center p-4 mx-auto md:p-8 mt-5">
+          <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">
+            Frequently Asked Questions
+          </h2>
+          <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
+            <details>
+              <summary className="py-2 outline-none cursor-pointer focus:underline">
+                What is the purpose of react router?
+              </summary>
+              <div className="px-4 pb-4">
+                <p>
+                  React Router, and dynamic, client-side routing, allows us to
+                  build a single-page web application with navigation without
+                  the page refreshing as the user navigates. React Router uses
+                  component structure to call components, which display the
+                  appropriate information.
+                </p>
+              </div>
+            </details>
+            <details>
+              <summary className="py-2 outline-none cursor-pointer focus:underline">
+                How does context API works?
+              </summary>
+              <div className="px-4 pb-4">
+                <p>
+                  The React Context API is a way for a React app to effectively
+                  produce global variables that can be passed around. This is
+                  the alternative to "prop drilling" or moving props from
+                  grandparent to child to parent, and so on. Context is also
+                  touted as an easier, lighter approach to state management
+                  using Redux.
+                </p>
+              </div>
+            </details>
+            <details>
+              <summary className="py-2 outline-none cursor-pointer focus:underline">
+                Write short note on useRef.
+              </summary>
+              <div className="px-4 pb-4 space-y-2">
+                <p>
+                The useRef Hook allows you to persist values between renders.It can be used to store a mutable value that does not cause a re-render when updated.It can be used to access a DOM element directly.
+                </p>
+              </div>
+            </details>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Blog;
--- quiz-website/src/components/Header/Header.js
+++ quiz-website/src/components/Header/Header.js
@@ -11,8 +11,6 @@ const Header = () => {
                 <Link to='/statistics'><span className='nav-item fs-5 text-dark fw-semibold p-3'>Statistics</span></Link>
                 <Link to='/blog'><span className='nav-item fs-5 text-dark fw-semibold p-3'>Blog</span></Link>
             </nav>
-            <h1 className='mt-5'>Welcome to the <span className='purple'>quiz community</span></h1>
-            <p className='mt-2 fs-4'>Learn and Practice here for free</p>
         </div>
     );
 };
